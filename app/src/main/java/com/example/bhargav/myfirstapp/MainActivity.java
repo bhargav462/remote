22ffdc5147ea8137181a0f3dcaabd50614fa8388
bhargav462 <bhargavnakkina462@gmail.com>
2019-02-12 18:48:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhargav/myfirstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bhargav.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+public class MainActivity extends AppCompatActivity {
+    public static final String MY_TAG = "the_custom_message";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(MY_TAG,"onCreate");
+    }
+}
