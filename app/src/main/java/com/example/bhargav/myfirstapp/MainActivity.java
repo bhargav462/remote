18fce66a82f7fc85962fd7d73d0dd8929cac9a00
bhargav462 <bhargavnakkina462@gmail.com>
2019-02-12 18:55:47 +0530

    nothing

--- app/src/main/java/com/example/bhargav/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/bhargav/myfirstapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(MY_TAG,"onCreate");
+        //another
     }
 }
